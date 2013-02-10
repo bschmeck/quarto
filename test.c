7e6a285ef7cb13f5e2e3327399065c845bdcf052
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2013-02-09 23:01:52 -0600

    Correctly return a value from the test suite.

--- test.c
+++ test.c
@@ -181,10 +181,17 @@ main(argc, argv)
 		char *argv;
 		int argc; 
 {
-		int ret;
+		int ret, t_ret;
 
-		ret = test_all_match();
-		printf("ALL_MATCH %s\n", ret == 0 ? "passes" : "fails");
+		ret = t_ret = 0;
+
+		t_ret = test_all_match();
+		printf("ALL_MATCH %s\n", t_ret == 0 ? "passes" : "fails");
+		ret = ret == 0 ? t_ret : ret;
+
 		ret = test_is_quarto();
-		printf("IS_QUARTO %s\n", ret == 0 ? "passes" : "fails");
+		printf("IS_QUARTO %s\n", t_ret == 0 ? "passes" : "fails");
+		ret = ret == 0 ? t_ret : ret;
+
+		return ret;
 }
