0c2595c34b26978b5a9cb61731c05d927119d164
Ben Schmeckpeper <ben@aisle50.com>
2013-02-20 07:52:06 -0600

    Fix expectation for test_choose_piece.

--- tests/test_choose_piece.c
+++ tests/test_choose_piece.c
@@ -35,7 +35,7 @@ test_choose_piece()
   ret = 0;
   
   CHOOSE_PIECE_TEST("../tests/test_files/cats_game.quarto", (WHITE | ROUND | HOLLOW | SHORT), "CHOOSE_PIECE CATS GAME");
-  CHOOSE_PIECE_TEST("../tests/test_files/score_move_one.quarto", (WHITE | ROUND | SOLID | TALL), "CHOOSE_PIECE SCORE MOVE ONE");
+  CHOOSE_PIECE_TEST("../tests/test_files/score_move_one.quarto", (BLACK | ROUND | HOLLOW | SHORT), "CHOOSE_PIECE SCORE MOVE ONE");
   CHOOSE_PIECE_TEST("../tests/test_files/two_moves_left.quarto", (WHITE | ROUND | HOLLOW | SHORT), "CHOOSE_PIECE TWO MOVES LEFT");
   
   return ret;
